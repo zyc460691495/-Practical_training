14c3195432ec860b9c1f98a00cf87f67ac63d921
zyc460691495 <460691495@qq.com>
2020-11-23 21:45:00 +0800

    1

--- JavaPro/src/com/zyc/view/impl/BusinessViewImpl.java
+++ JavaPro/src/com/zyc/view/impl/BusinessViewImpl.java
@@ -17,10 +17,12 @@ public class BusinessViewImpl implements BusinessView {
         BusinessDao dao = new BusinessDaoImpl();
         List<Business> list = dao.listBusiness(null, null);
 
+        System.out.format("%-10s %20s %25s %25s %25s\n", "商家编号", "商家名称", "商家地址", "配送费", "起送价格");
         for (Business b : list) {
-            System.out.println(b.getBusinessId() + "\t" + b.getBusinessName() +
-                    "\t" + b.getBusinessAddress() + "\t" + b.getBusinessExplain() +
-                    "\t" + b.getDeliveryPrice() + "\t" + b.getStarPrice());
+            System.out.format("%-10d %23s %25s %20s %20.2f", b.getBusinessId(), b.getBusinessName(),
+                    b.getBusinessAddress(), b.getBusinessExplain(),
+                    b.getDeliveryPrice(), b.getStarPrice());
+            System.out.println();
         }
     }
 
@@ -42,42 +44,42 @@ public class BusinessViewImpl implements BusinessView {
             businessAdress = input.next();
         }
         List<Business> list = dao.listBusiness(businessName, businessAdress);
-//        System.out.format("%s %s %s %s %s %s","商家编号","商家名称","商家地址","","配送费","商品价格");
-//        System.out.format("%d %s %s %s %a %a","");
+        System.out.format("%-10s %20s %25s %25s %25s\n", "商家编号", "商家名称", "商家地址", "配送费", "起送价格");
         for (Business b : list) {
-            System.out.println(b.getBusinessId() + "\t" + b.getBusinessName() +
-                    "\t" + b.getBusinessAddress() + "\t" + b.getBusinessExplain() +
-                    "\t" + b.getDeliveryPrice() + "\t" + b.getStarPrice());
+            System.out.format("%-10d %23s %25s %20s %20.2f", b.getBusinessId(), b.getBusinessName(),
+                    b.getBusinessAddress(), b.getBusinessExplain(),
+                    b.getDeliveryPrice(), b.getStarPrice());
+            System.out.println();
         }
     }
 
     @Override
     public void addBusiness() {
-        BusinessDao dao=new BusinessDaoImpl();
+        BusinessDao dao = new BusinessDaoImpl();
         System.out.println("请输入新商家的名称");
         String businessName = input.next();
         System.out.println("请输入新商家的密码");
-        String businessPassword=input.next();
-        int businessId = dao.addBusiness(businessName,businessPassword);
+        String businessPassword = input.next();
+        int businessId = dao.addBusiness(businessName, businessPassword);
         // 根据id进行查询， 然后进行回显
-        if (businessId >0){
+        if (businessId > 0) {
             System.out.println("保存成功");
             Business business = dao.getBusinessById(businessId);
             System.out.println(business);
-        }else {
+        } else {
             System.out.println("新建商家失败");
         }
     }
 
     @Override
     public void removeBusiness() {
-        BusinessDao dao=new BusinessDaoImpl();
+        BusinessDao dao = new BusinessDaoImpl();
         System.out.println("请输入要移除商家的编号");
-        Integer businessId=input.nextInt();
+        Integer businessId = input.nextInt();
         dao.removeBusiness(businessId);
         System.out.println("是否移除（yes/no）");
-        String flag=input.next();
-        if(flag.equals("yes")){
+        String flag = input.next();
+        if (flag.equals("yes")) {
             System.out.println("商家已移除");
         }
     }
@@ -85,88 +87,88 @@ public class BusinessViewImpl implements BusinessView {
     @Override
     public Business login() {
         System.out.println("请输入商家编号");
-        Integer businessId=input.nextInt();
+        Integer businessId = input.nextInt();
         System.out.println("请输入密码");
-        String password=input.next();
-        BusinessDao businessDao=new BusinessDaoImpl();
-        return businessDao.getBusinessByIdAndPassword(businessId,password);
+        String password = input.next();
+        BusinessDao businessDao = new BusinessDaoImpl();
+        return businessDao.getBusinessByIdAndPassword(businessId, password);
     }
 
     @Override
     public void updateBusiness(Business business) {
         System.out.println(business.toString());
-        String flag="";
-        String businessName=business.getBusinessName();
-        String businessAddress=business.getBusinessAddress();
-        String businessExplain=business.getBusinessExplain();
-        Double startPrice=business.getDeliveryPrice();
-        Double deliveryPrice=business.getDeliveryPrice();
+        String flag = "";
+        String businessName = business.getBusinessName();
+        String businessAddress = business.getBusinessAddress();
+        String businessExplain = business.getBusinessExplain();
+        Double startPrice = business.getDeliveryPrice();
+        Double deliveryPrice = business.getDeliveryPrice();
         System.out.println("是否更改商家名称（yes/no）");
-        flag=input.next();
-        if(flag.equals("yes")){
+        flag = input.next();
+        if (flag.equals("yes")) {
             System.out.println("请输入新的商家名称");
-            businessName=input.next();
+            businessName = input.next();
             business.setBusinessName(businessName);
         }
         System.out.println("是否更改商家地址（yes/no）");
-        flag=input.next();
-        if(flag.equals("yes")){
+        flag = input.next();
+        if (flag.equals("yes")) {
             System.out.println("请输入新的商家地址");
-            businessAddress=input.next();
+            businessAddress = input.next();
             business.setBusinessAddress(businessAddress);
         }
         System.out.println("是否更改商家介绍（yes/no）");
-        flag=input.next();
-        if(flag.equals("yes")){
+        flag = input.next();
+        if (flag.equals("yes")) {
             System.out.println("请输入新的商家介绍");
-            businessExplain=input.next();
+            businessExplain = input.next();
             business.setBusinessAddress(businessExplain);
-        }else {
+        } else {
 
         }
         System.out.println("是否更改起送价格（yes/no）");
-        flag=input.next();
-        if(flag.equals("yes")){
+        flag = input.next();
+        if (flag.equals("yes")) {
             System.out.println("请输入新的起送价格");
-            startPrice=input.nextDouble();
+            startPrice = input.nextDouble();
             business.setStarPrice(startPrice);
         }
         System.out.println("是否更改配送价格（yes/no）");
-        flag=input.next();
-        if(flag.equals("yes")){
+        flag = input.next();
+        if (flag.equals("yes")) {
             System.out.println("请输入新的配送价格");
-            deliveryPrice=input.nextDouble();
+            deliveryPrice = input.nextDouble();
             business.setDeliveryPrice(deliveryPrice);
         }
-        BusinessDao businessDao=new BusinessDaoImpl();
-        int res=businessDao.updateBusiness(business);
-        if (res==1){
+        BusinessDao businessDao = new BusinessDaoImpl();
+        int res = businessDao.updateBusiness(business);
+        if (res == 1) {
             System.out.println("修改成功");
-        }else {
+        } else {
             System.out.println("修改失败");
         }
     }
 
     @Override
     public void updateBusinessPassword(Integer businessId) {
-        BusinessDao businessDao=new BusinessDaoImpl();
-        Business business=businessDao.getBusinessById(businessId);
+        BusinessDao businessDao = new BusinessDaoImpl();
+        Business business = businessDao.getBusinessById(businessId);
         System.out.println("请输入旧密码");
-        String password=input.next();
-        if(!password.equals(business.getPassword())){
+        String password = input.next();
+        if (!password.equals(business.getPassword())) {
             System.out.println("旧密码错误！！！");
-        }else {
+        } else {
             System.out.println("请输入新密码");
-            String newpassword=input.next();
+            String newpassword = input.next();
             System.out.println("请再次输入新密码");
-            if(input.next().equals(newpassword)){
-                int rs=businessDao.updateBusinessPassword(businessId,newpassword);
-                if (rs>0){
+            if (input.next().equals(newpassword)) {
+                int rs = businessDao.updateBusinessPassword(businessId, newpassword);
+                if (rs > 0) {
                     System.out.println("修改密码成功");
-                }else {
+                } else {
                     System.out.println("修改密码失败");
                 }
-            }else {
+            } else {
                 System.out.println("两次密码输入不一致");
             }
         }
--- JavaPro/src/com/zyc/view/impl/FoodViewImpl.java
+++ JavaPro/src/com/zyc/view/impl/FoodViewImpl.java
@@ -13,38 +13,40 @@ import java.util.Scanner;
 
 public class FoodViewImpl implements FoodView {
     private Scanner input = new Scanner(System.in);
+
     @Override
     public void showFoodList(Integer businessId) {
         FoodDao dao = new FoodDaoImpl();
         List<Food> list = dao.listFoodByBusinessId(businessId);
 
+        System.out.format("%-10s %20s %20s %20s %20s\n","食品编号","食品名称","食品介绍","食品价格","商家编号");
         for (Food f : list) {
-            System.out.println(f.getFoodId() + "\t" + f.getFoodName()+
-                    "\t" + f.getFoodExplain() + "\t" + f.getFoodPrice() +
-                    "\t" + f.getBusinessId());
+            System.out.format("%-10d %25s %15s %23f %21d", f.getFoodId(), f.getFoodName(),
+                    f.getFoodExplain(), f.getFoodPrice(), f.getBusinessId());
+            System.out.println();
         }
     }
 
     @Override
     public void saveFood(Integer businessId) {
-        FoodDao dao=new FoodDaoImpl();
-        Food food =new Food();
+        FoodDao dao = new FoodDaoImpl();
+        Food food = new Food();
         System.out.println("请输入新食品的名称");
         String foodName = input.next();
         System.out.println("请输入新食品的价格");
-        Double foodPrice=input.nextDouble();
+        Double foodPrice = input.nextDouble();
 
         food.setFoodName(foodName);
         food.setFoodPrice(foodPrice);
         food.setBusinessId(businessId);
 
-        int foodId= dao.saveFood(food);
+        int foodId = dao.saveFood(food);
         // 根据id进行查询， 然后进行回显
-        if (foodId >0){
+        if (foodId > 0) {
             System.out.println("保存成功");
             Food food1 = dao.getFoodById(foodId);
             System.out.println(food1);
-        }else {
+        } else {
             System.out.println("新建食品失败");
         }
     }
@@ -54,44 +56,44 @@ public class FoodViewImpl implements FoodView {
 
         showFoodList(businessId);
         System.out.println("请输入要修改食品的编号");
-        Integer foodId=input.nextInt();
-        FoodDao foodDao=new FoodDaoImpl();
-        if (businessId.intValue()!=foodDao.getFoodById(foodId).getBusinessId().intValue()){
+        Integer foodId = input.nextInt();
+        FoodDao foodDao = new FoodDaoImpl();
+        if (businessId.intValue() != foodDao.getFoodById(foodId).getBusinessId().intValue()) {
             System.out.println("店内没有该编号的食品");
             return;
         }
-        Food food= foodDao.getFoodById(foodId);
+        Food food = foodDao.getFoodById(foodId);
 
-        String flag="";
-        String foodName=food.getFoodName();
-        String foodExplain=food.getFoodExplain();
-        Double foodPrice=food.getFoodPrice();
+        String flag = "";
+        String foodName = food.getFoodName();
+        String foodExplain = food.getFoodExplain();
+        Double foodPrice = food.getFoodPrice();
         System.out.println("是否更改食品名称（yes/no）");
-        flag=input.next();
-        if(flag.equals("yes")){
+        flag = input.next();
+        if (flag.equals("yes")) {
             System.out.println("请输入新的食品名称");
-            foodName=input.next();
+            foodName = input.next();
             food.setFoodName(foodName);
         }
         System.out.println("是否更改食品介绍（yes/no）");
-        flag=input.next();
-        if(flag.equals("yes")){
+        flag = input.next();
+        if (flag.equals("yes")) {
             System.out.println("请输入新的食品介绍");
-            foodExplain=input.next();
+            foodExplain = input.next();
             food.setFoodExplain(foodExplain);
         }
         System.out.println("是否更改食品价格（yes/no）");
-        flag=input.next();
-        if(flag.equals("yes")){
+        flag = input.next();
+        if (flag.equals("yes")) {
             System.out.println("请输入新的食品价格");
-            foodPrice=input.nextDouble();
+            foodPrice = input.nextDouble();
             food.setFoodPrice(foodPrice);
         }
         System.out.println(food.toString());
-        int res=foodDao.updateFood(food);
-        if (res==1){
+        int res = foodDao.updateFood(food);
+        if (res == 1) {
             System.out.println("修改成功");
-        }else {
+        } else {
             System.out.println("修改失败");
         }
     }
@@ -99,18 +101,18 @@ public class FoodViewImpl implements FoodView {
     @Override
     public void removeFood(Integer businessId) {
         showFoodList(businessId);
-        FoodDao dao=new FoodDaoImpl();
+        FoodDao dao = new FoodDaoImpl();
         System.out.println("请输入要移除食品的编号");
-        Integer foodId=input.nextInt();
+        Integer foodId = input.nextInt();
 
-        if (businessId.intValue()!=dao.getFoodById(foodId).getBusinessId().intValue()){
+        if (businessId.intValue() != dao.getFoodById(foodId).getBusinessId().intValue()) {
             System.out.println("店内没有该编号的食品");
             return;
         }
         dao.removeFood(foodId);
         System.out.println("是否移除（yes/no）");
-        String flag=input.next();
-        if(flag.equals("yes")){
+        String flag = input.next();
+        if (flag.equals("yes")) {
             System.out.println("食品已移除");
         }
     }
