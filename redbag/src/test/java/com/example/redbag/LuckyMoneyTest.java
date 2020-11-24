da564928e36cfdf23e3532394413109109d4bb52
zyc460691495 <460691495@qq.com>
2020-11-24 19:15:04 +0800

    1

--- redbag/src/test/java/com/example/redbag/LuckyMoneyTest.java
+++ redbag/src/test/java/com/example/redbag/LuckyMoneyTest.java
@@ -11,9 +11,10 @@ import java.util.List;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class LuckyMoneyTest {
-    @Autowired
+    @Autowired//对成员变量、方法和构造函数进行标注，来完成自动装配的工作
     private LuckMoneyRepo repo;
-    @Test
+    @Test//不用new就可以执行方法
+    // 指定将会抛出的异常类型，如果没有抛出异常或者抛出的异常不属于指定的类型，就会算是测试失败了 测试一断代码运行时间
     public void listAll(){
 
         List<LuckyMoney> list=repo.findAll();
